c97f4d0cbd24e9c4d829383b7edf513020e7dafc
Dossey <richardsdossey@gmail.com>
2017-05-13 20:35:10 -0400

    inital commit

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var shell = require('shelljs');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+
+router.get('/move-forward', function(req, res, next) {
+  shell.exec('goforward.py');
+});
+
+
+module.exports = router;
